987160002b156eb8fef7fb72bc7ed7f0ef1c4bbb
sisira <sisira3128@gmail.com>
2023-08-17 22:02:35 -0400

    no changes

--- wk5U3addparams.py
+++ wk5U3addparams.py
@@ -6,13 +6,12 @@ Implement a for loop which iterates over the range(100).
  Within the for loop, the sequence-variable is checked with the function is_even().
  Depending on the return value, either x is even or x is not even is printed."""
 def is_even(number):
-    if number%2==0:
-         return True
-    else :
-         return False
-      
+    if number%2 == 0:
+        return True
+    else:
+        return False     
 for x in range(int(input("Enter number:"))):
     if is_even(x):
-        print(x,"is even")
-    else :
-        print(x,"is not even")
+        print(x, "is even")
+    else:
+        print(x, "is not even")
